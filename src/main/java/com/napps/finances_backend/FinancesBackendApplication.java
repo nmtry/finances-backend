03b3c84447e2f2e4a9709a41cc3ce68f0f25a064
nick tryon <nmtryon@verizon.net>
2025-11-22 21:43:44 -0500

    initial setup

---
+++ src/main/java/com/napps/finances_backend/FinancesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.napps.finances_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/napps/finances_backend/FinancesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napps.finances_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
